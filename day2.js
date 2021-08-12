e122d125213127c866335ec7145d6f7ff5aed4cc
AzimuthAZ <88020706+AzimuthAZ@users.noreply.github.com>
2021-08-12 13:45:35 +0700

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,60 @@
+// var destination = "office hq";
+// var quantity = 3;
+// var food = "ayam";
+// destination = "cabang";
+// console.log(destination);
+// console.log(destination);
+// console.log(destination);
+// console.log("office hq");
+// console.log("office hq");
+// console.log("office hq");
+// Name disini gunanya untuk nyimpen nama user
+// comment
+// var name = "john";
+// var name = "jane";
+// console.log(name);
+// console.log(name);
+
+// var angka1 = 6 + 5 * 2
+// var angka2 = 10
+
+// var angka3 = angka1 / angka2
+// console.log(angka3)
+// angka1 --
+// console.log(angka1)
+
+// console.log(Math.abs(-90))
+// console.log(Math.PI)
+// console.log(Math.cbrt(2,4))
+
+// console.log(Math.random(1,10))
+
+// var multiTen = Math.random()* 10
+// var randomnum = Math.floor(multiTen)
+// console.log(randomNum)
+// var randomNum = Math.floor(multiTen) + 1
+// console.log(randomNum)
+
+// var buah = "mangga"
+// var buah1 = "apel"
+// var buah2 = buah + buah1
+// console.log(buah2)
+
+// var nama = 'john'
+// var kata = 'Wait, "I\'ll be next"'
+// console.log(kata)
+// var kata2 = `wait, "I'll be next" ${nama}` // /n$ /t$
+// console.log(kata2)
+
+var firstName = 'John Legend'
+
+console.log(firstName.length)
+console.log(firstName.indexOf('Legend'))
+console.log(firstName.substr(1, 2))
+console.log(firstName.slice(1, 7))
+console.log(firstName.split(''))
+console.log(firstName.replace('o', 'p'))
+
+// var lastName = 'Wick'
+// var fullName = `my full name is ${firstName} ${lastName}`
+// console.log(fullName)
